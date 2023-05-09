f69a3730208968094b4ab936315c2a7c632e3a83
geisonbruno1 <geisonbruno0@gmail.com>
2023-05-08 22:01:56 -0300

    Project created

---
+++ src/main/java/com/br/listgame/ListgameApplication.java
@@ -0,0 +1,13 @@
+package com.br.listgame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListgameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ListgameApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/listgame/ListgameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.listgame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListgameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
